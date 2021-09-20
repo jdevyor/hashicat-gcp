f4c9f1c68560066d9b363e2235c2449c0ebf5b40
jdevyor <jdevyor@amfam.com>
2021-09-20 19:51:35 +0000

    Added VPC Module-5

--- vcp.tf
+++ vcp.tf
@@ -2,13 +2,13 @@ module "network" {
   source  = "terraform-google-modules/network/google"
   version = "3.4.0"
   network_name = "devyor-vpc"
-  project_id   = "var.project"
+  project_id   = var.project
 
   subnets = [
         {
             subnet_name           = "devyorsubnet"
             subnet_ip             = "10.10.10.0/24"
-            subnet_region         = "var.region"
+            subnet_region         = var.region
         }
     ]
 }
